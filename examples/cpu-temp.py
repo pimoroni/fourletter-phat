84b206ec3800ca25938f75e0c1e070bee5f1a545
sandyjmacdonald <sandyjmacdonald@gmail.com>
2017-03-11 20:32:05 +0000

    Made cpu-temp.py a bit less twitchy

--- examples/cpu-temp.py
+++ examples/cpu-temp.py
@@ -4,6 +4,8 @@ import time
 import fourletterphat as flp
 from subprocess import Popen, PIPE
 
+temps = []
+
 while True:
     temp = Popen(["vcgencmd", "measure_temp"], stdout=PIPE)
     temp = temp.stdout.read().decode('utf-8')
@@ -12,4 +14,4 @@ while True:
     flp.print_str(temp)
     flp.set_decimal(1, 1)
     flp.show()
-    time.sleep(0.05)
+    time.sleep(1)
