af526755eef5a6e655ce90d1950dd2590838e571
Phil Howard <phil@gadgetoid.com>
2017-04-27 09:09:58 +0000

    Added demo of blink functionality

---
+++ examples/blink.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import time
+import fourletterphat
+
+print("""
+Four Letter pHAT: blink.py
+
+Demonstrate the display blinking at
+the three available speeds.
+
+Press Ctrl+C to exit.
+""")
+
+fourletterphat.clear()
+fourletterphat.print_str("BLNK")
+fourletterphat.show()
+
+for blink_speed in [fourletterphat.HT16K33_BLINK_HALFHZ, fourletterphat.HT16K33_BLINK_1HZ, fourletterphat.HT16K33_BLINK_2HZ]:
+    fourletterphat.set_blink(blink_speed)
+    time.sleep(4)
+
+fourletterphat.set_blink(fourletterphat.HT16K33_BLINK_OFF)
+
+fourletterphat.print_str("BOOM")
+fourletterphat.show()
+time.sleep(1)
