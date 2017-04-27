b31c16095b7f6f2fcd491c92988975ed65c9c06c
Phil Howard <phil@gadgetoid.com>
2017-04-27 09:08:49 +0000

    Added set_blink and set_brightness

--- library/fourletterphat/__init__.py
+++ library/fourletterphat/__init__.py
@@ -2,6 +2,7 @@ import atexit
 from sys import exit, version_info
 from .i2c_bus import bus
 from .alphanum4 import AlphaNum4
+from .HT16K33 import *
 
 __version__ = '0.0.1'
 
@@ -10,6 +11,8 @@ display.begin()
 display.clear()
 display.show()
 
+set_blink = display.set_blink
+set_brightness = display.set_brightness
 set_digit_raw = display.set_digit_raw
 set_decimal = display.set_decimal
 set_digit = display.set_digit
