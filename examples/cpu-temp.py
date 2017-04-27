f346b5424ed42ac7dbb83d4ffee1b5100c323e9e
Phil Howard <phil@gadgetoid.com>
2017-04-27 09:33:36 +0000

    Dropped unecessary var, added comment

--- examples/cpu-temp.py
+++ examples/cpu-temp.py
@@ -14,14 +14,17 @@ Press Ctrl+C to exit.
 """)
 
 
-temps = []
-
 while True:
+    # Get temp forom vcgencmd in the format: "temp=XY.Z'C"
+    # and reduce to the format "XYZC" for display
     temp = Popen(["vcgencmd", "measure_temp"], stdout=PIPE)
     temp = temp.stdout.read().decode('utf-8')
     temp = temp[5:].replace(".", "").replace("'","").strip()
+
     fourletterphat.clear()
     fourletterphat.print_str(temp)
     fourletterphat.set_decimal(1, 1)
     fourletterphat.show()
+
     time.sleep(1)
+
